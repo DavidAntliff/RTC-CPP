cea541854f56d27aefc794ea5867c6a022706def
David Antliff <david.antliff@gmail.com>
2023-03-28 18:19:45 +1300

    Chapter 10 - Patterns: nested patterns (tiled floor).

---
+++ src/chapter10_tiled_floor.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <numbers>
+
+#include <ray_tracer_challenge/matrices.h>
+#include <ray_tracer_challenge/tuples.h>
+#include <ray_tracer_challenge/canvas.h>
+#include <ray_tracer_challenge/transformations.h>
+#include <ray_tracer_challenge/spheres.h>
+#include <ray_tracer_challenge/planes.h>
+#include <ray_tracer_challenge/lights.h>
+#include <ray_tracer_challenge/materials.h>
+#include <ray_tracer_challenge/world.h>
+#include <ray_tracer_challenge/camera.h>
+#include <ray_tracer_challenge/patterns.h>
+
+using namespace rtc;
+
+using namespace std::numbers;
+
+int main(int argc, char * argv[]) {
+    (void)argc;
+    (void)argv;
+
+    auto w = world();
+
+    auto floor = plane();
+    floor.set_material(material());
+    floor.material().set_color(color(1.0, 0.9, 0.9));
+    floor.material().set_specular(0.0);
+    auto const scale {0.3};
+    auto floor_pattern_1 = stripe_pattern(color(167/255.0, 83/255.0, 104/255.0), color(124/255.0, 41/255.0, 62/255.0));
+    floor_pattern_1.set_transform(scaling(scale, scale, scale).then(rotation_y(pi / 4.0)));
+    auto floor_pattern_2 = stripe_pattern(color(63/255.0, 63/255.0, 63/255.0), color(104/255.0, 104/255.0, 104/255.0));
+    floor_pattern_2.set_transform(scaling(scale, scale, scale).then(rotation_y(-pi / 4.0)));
+    auto floor_pattern = checkers_pattern(floor_pattern_1, floor_pattern_2);
+    set_pattern_transform(floor_pattern, scaling(1.0, 1.0, 1.0));
+    floor.material().set_pattern(floor_pattern);
+    w.add_object(floor);
+
+    w.add_light(point_light(point(-10.0, 10.0, -10.0), color(1.0, 1.0, 1.0)));
+
+    //auto cam = camera(100, 50, pi / 3.0);
+    //auto cam = camera(400, 200, pi / 3.0);
+    //auto cam = camera(1024, 768, pi / 3.0);
+    auto cam = camera(2048, 1536, pi / 3.0);
+    cam.set_transform(view_transform(point(0.0, 1.5, -5.0),
+                                     point(0.0, 1.0, 0.0),
+                                     vector(0.0, 1.0, 0.0)));
+
+    auto canvas = render(cam, w);
+
+    auto ppm = ppm_from_canvas(canvas);
+    std::cout << ppm;
+
+    return 0;
+}
